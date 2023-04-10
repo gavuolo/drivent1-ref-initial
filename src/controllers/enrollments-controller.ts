638dd5a148502168015474078d1562540d636557
gavuolo <gabs.vuolo@gmail.com>
2023-04-10 12:05:49 -0400

    Feat: teste

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import enrollmentsService from '@/services/enrollments-service';
+import { QueryCEP } from '@/protocols';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -29,12 +30,23 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 }
 
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  const { cep } = req.query as QueryCEP;
+
   try {
-    const address = await enrollmentsService.getAddressFromCEP();
-    res.status(httpStatus.OK).send(address);
+    const { logradouro, complemento, bairro, cidade, uf } = await enrollmentsService.getAddressFromCEP(cep);
+    const responseData = {
+      logradouro,
+      complemento,
+      bairro,
+      cidade,
+      uf,
+    };
+    res.status(httpStatus.OK).send(responseData);
   } catch (error) {
     if (error.name === 'NotFoundError') {
+      console.log('oi');
       return res.send(httpStatus.NO_CONTENT);
     }
+    res.sendStatus(500);
   }
 }
